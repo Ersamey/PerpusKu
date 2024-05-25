4592fa537db21af7b6fd3f3182a606750667e64d
Ersamey <meiliaersa@gmail.com>
2024-05-25 11:58:41 +0700

    update edit review

--- app/Views/pages/user/index.php
+++ app/Views/pages/user/index.php
@@ -60,9 +60,9 @@
                         <th>Review</th>
                         <th>Action</th>
                     </tr>
-                    <?php foreach ($review as $r) : ?>
+                    <?php foreach ($review as $index => $r) : ?>
                         <tr>
-                            <td><?= $i++; ?></td>
+                            <td><?= $index + 1; ?></td>
                             <td><?= $r['judul']; ?></td>
                             <td>
                                 <?php
@@ -72,48 +72,53 @@
                                     echo $r['review'];
                                 }
                                 ?>
-
-                                <div id="sembunyi" style="display: none;">
+                                <div id="sembunyi-<?= $r['id_review']; ?>" style="display: none;">
                                     <form action="/review/edit" method="post" enctype="multipart/form-data">
                                         <div class="card-footer py-3 border-0">
                                             <div class=" d-flex flex-start w-100">
                                                 <div data-mdb-input-init class="form-outline w-100">
-                                                    <textarea class="form-control" name="review" id="review" rows="4" value="<?= $r['review']; ?>"></textarea>
+                                                    <textarea class="form-control" name="review" id="review" rows="4"><?= $r['review']; ?></textarea>
                                                 </div>
                                             </div>
                                         </div>
                                         <input type="hidden" id="id_review" name="id_review" value="<?= $r['id_review']; ?>">
                                         <input type="hidden" id="user_id" name="user_id" value="<?= $r['user_id']; ?>">
-                                        <script>
-                                            function myfunc() {
-                                                var x = document.getElementById("sembunyi");
-                                                if (x.style.display != "none") {
-                                                    x.style.display = "none";
-                                                } else {
-                                                    x.style.display = "block";
-                                                }
-                                            }
-                                        </script>
                                         <div class="float-end mt-2 pt-1">
                                             <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-sm">Simpan</button>
-                                            <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-primary btn-sm">Batal</button>
+                                            <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-primary btn-sm" onclick="hideForm('sembunyi-<?= $r['id_review']; ?>')">Batal</button>
                                         </div>
                                     </form>
                                 </div>
                             </td>
                             <td>
-                                <!-- <a href="/review/edit/" class="btn btn-warning">Edit</a> -->
-                                <button class="btn btn-warning" onclick="myfunc()" class="mt-3">Edit review</button>
+                                <button class="btn btn-warning" onclick="showForm('sembunyi-<?= $r['id_review']; ?>')" class="mt-3">Edit review</button>
 
                                 <form action="/user/<?= $r['id_review']; ?>" method="post">
                                     <?= csrf_field(); ?>
                                     <input type="hidden" name="_method" value="DELETE">
-                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Apakkah anda yakin?');">Delete</button>
+                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Apakah anda yakin?');">Delete</button>
                                 </form>
                             </td>
                         </tr>
                     <?php endforeach; ?>
                 </table>
+
+                <script>
+                    function showForm(id) {
+                        var x = document.getElementById(id);
+                        if (x.style.display === "none") {
+                            x.style.display = "block";
+                        } else {
+                            x.style.display = "none";
+                        }
+                    }
+
+                    function hideForm(id) {
+                        var x = document.getElementById(id);
+                        x.style.display = "none";
+                    }
+                </script>
+
             </div>
 
         </div>
