8d2bfb27727310bf21c91d44bbd40c1411e51bb9
Ersamey <meiliaersa@gmail.com>
2024-06-05 18:33:17 +0700

    FE login dan register

--- app/Views/auth/signIn.php
+++ app/Views/auth/signIn.php
@@ -1,42 +1,44 @@
 <?= $this->extend('layout/template'); ?>
+
 <?= $this->section('content'); ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<form class="user">
-    <div class="form-group">
-        <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-    </div>
-    <div class="form-group">
-        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-    </div>
-    <div class="form-group">
-        <div class="custom-control custom-checkbox small">
-            <input type="checkbox" class="custom-control-input" id="customCheck">
-            <label class="custom-control-label" for="customCheck">Remember
-                Me</label>
+<div class="login">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 28rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Welcome to Perpusku!</h5>
+                        <?= view('Myth\Auth\Views\_message_block') ?>
+                        <form action="<?= url_to('login') ?>" method="post">
+                            <?= csrf_field() ?>
+                            <div class="form-group">
+                                <label for="">Email or username</label>
+                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Password</label>
+                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                            </div>
+
+                            <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                Login
+                            </a>
+                            <hr>
+                        </form>
+                        <hr>
+
+                        <div class="text-center">
+                            <p>Do you want to create an account?<a class="small" href="/register">Sign up</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <a href="index.html" class="btn btn-primary btn-user btn-block">
-        Login
-    </a>
-    <hr>
-    <a href="index.html" class="btn btn-google btn-user btn-block">
-        <i class="fab fa-google fa-fw"></i> Login with Google
-    </a>
-    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-    </a>
-</form>
-<hr>
-<div class="text-center">
-    <a class="small" href="forgot-password.html">Forgot Password?</a>
-</div>
-<div class="text-center">
-    <a class="small" href="register.html">Create an Account!</a>
 </div>
 
-</body>
 
-</html>
+
+<?= $this->endSection(); ?>
